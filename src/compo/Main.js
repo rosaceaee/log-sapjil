17d17cecb154fe8b45b550bc5fba8671f374036d
j <115390541+rosaceaee@users.noreply.github.com>
2024-09-03 16:40:57 +0900

    draft works sabori

--- src/compo/Main.js
+++ src/compo/Main.js
@@ -1,18 +1,85 @@
-import React from "react";
-import Nav from "./Nav";
-import Article from "./Article";
+import React, { useState, useEffect } from "react";
+import { Button, Col, Row, Switch, Space, Menu } from "antd";
+import { HeartFilled } from "@ant-design/icons";
+
+import Typing from "./Typing";
+import "../style/index.scss";
 
 const Main = () => {
-    return(
-        <>
-            <Nav />
-            <article>
-                <Article/>
-            </article>
+  const [txt, setTxt] = useState("");
 
-            
-        </>
-    )
-}
+  const menulist = [{ label: "a" }, { label: "b" }];
+  const getData = (item) => {
+    const bookmarkList = localStorage.getItem(".ql-editor") || "[]";
+    const parsedList = JSON.parse(bookmarkList);
+    const newItem = {
+      title: item.title,
+      txt: item.txt,
+    };
+    const newList = [...parsedList, newItem];
+
+    // get a data
+
+    localStorage.setItem("bookmark", JSON.stringify());
+  };
+
+  useEffect(() => {
+    const savedText = localStorage.getItem("txt");
+    if (savedText) {
+      setTxt(savedText);
+    }
+  }, []);
+
+  const saveit = () => {
+    localStorage.setItem("txt", txt);
+    alert("y");
+  };
+
+  const transfer = [txt, setTxt];
+
+  return (
+    <>
+      <Menu items={menulist} mode="horizontal" />
+      <Row className="content-wrap">
+        <Col span={1}>
+          <div className="nav-wrap">
+            <div>
+              <HeartFilled />
+            </div>
+            <div>
+              <HeartFilled />
+            </div>
+            <div>
+              <HeartFilled />
+            </div>
+          </div>
+        </Col>
+        <Col span={3}>
+          <div className="menu-wrap">
+            <h3>navi1</h3>
+            <h3>navi1`</h3>
+            <h3>navi111</h3>
+          </div>
+        </Col>
+        <Col span={17}>
+          <div className="naiyou-wrap">
+            <p>asdf</p> <p>asdf</p>
+          </div>
+          <Row>
+            <Col span={24}>
+              <Typing transfer={transfer} />
+            </Col>
+            {/* <Col span={24}>
+              <Button onClick={saveit}>Send</Button>
+            </Col> */}
+          </Row>
+        </Col>
+        <Col span={3}>
+          <div className="tl-wrap">timeline</div>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default Main;
--- src/compo/Typing.js
+++ src/compo/Typing.js
@@ -1,19 +1,45 @@
-import React from "react";
-import { useQuill } from 'react-quilljs';
+import React, { useState, useRef, useEffect } from "react";
 
-import 'quill/dist/quill.snow.css'; // Add css for snow theme
+import { Button, Col, Row, Switch, Space, Menu } from "antd";
+import ReactDOM from "react-dom";
+import { Editor, EditorState, RichUtils, Immutable } from "draft-js";
+import "draft-js/dist/Draft.css";
 
 const Typing = () => {
-    const { quill, quillRef } = useQuill();
+  const [value, setValue] = useState("");
+  const onChange = (evt) => setValue(evt.target.value);
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
 
-  console.log(quill);    // undefined > Quill Object
-  console.log(quillRef); // { current: undefined } > { current: Quill Editor Reference }
+  const Blockkk = (propp) => {
+    return (
+      <>
+        <div className="block-wrap">{this.propp}</div>
+      </>
+    );
+  };
+
+  // const blockRenderMap = Immutable.Map({
+  //   MyCustomBlock: {
+  //     // element is used during paste or html conversion to auto match your component;
+  //     // it is also retained as part of this.props.children and not stripped out
+  //     element: "section",
+  //     wrapper: <Blockkk />,
+  //   },
+  // });
 
   return (
-    <div style={{ width: '100%', height: 300 }}>
-      <div ref={quillRef} />
+    <div style={{ width: "100%", height: 300 }}>
+      {/* <input value={value} onChange={onChange} />; */}
+      <Editor
+        editorState={editorState}
+        textAlignment="center"
+        onChange={setEditorState}
+      />
+      ;
     </div>
   );
-}
+};
 
 export default Typing;
